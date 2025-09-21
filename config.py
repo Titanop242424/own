07804639942d0c0d8b3710ab8b9b367bf1719f45
Titanop242424 <optitan317@gmail.com>
2025-09-21 20:43:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+# Configuration settings
+
+# Telegram Bot Token
+TELEGRAM_TOKEN = "7788865701:AAHFVFbSdhpRuMTmLj987J8BmwKLR3j4brk"
+
+# Admin User ID
+ADMIN_ID = 7163028849
+
+# Bot Owner Name
+BOT_OWNER = "TITAN FREE DDoS 🚀 BOT"
+
+# GitHub Tokens (comma separated)
+GITHUB_TOKENS = ["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", "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", "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", "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"]
+
+# Approved users list
+APPROVED_USERS = [ADMIN_ID]
